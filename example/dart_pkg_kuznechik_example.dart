32d94fbb44abc339a6e12b4d38a84432fc2d2a66
slermo <ledumblasphemus@gmail.com>
2024-05-01 19:33:44 +0300

    init

---
+++ example/dart_pkg_kuznechik_example.dart
@@ -0,0 +1,13 @@
+import 'dart:ffi';
+
+import 'package:dart_pkg_kuznechik/dart_pkg_kuznechik.dart';
+
+import '../kuznechik_bindings.dart';
+
+void main() {
+  const path =
+      'D:/Slermo/SlermosFiles/Voenmech/ВКР/programming/dart_pkg_kuznechik/kuznechik';
+  Kuznechik(DynamicLibrary.open(path));
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/dart_pkg_kuznechik_test.dart
@@ -0,0 +1,16 @@
+import 'package:dart_pkg_kuznechik/dart_pkg_kuznechik.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
