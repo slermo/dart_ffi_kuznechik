914d6f2d4b2688220524415852f44ef6223a764e
Svyatoslav <ermolaev@spiks.ru>
2025-07-09 13:19:51 +0300

    feature: fixes

--- example/dart_pkg_kuznechik_example.dart
+++ example/dart_pkg_kuznechik_example.dart
@@ -1,12 +1,11 @@
 import 'dart:ffi';
 
-import 'package:dart_pkg_kuznechik/dart_pkg_kuznechik.dart';
+import 'package:ffi_kuznechik/dart_pkg_kuznechik.dart';
 
 import '../kuznechik_bindings.dart';
 
 void main() {
-  const path =
-      'D:/Slermo/SlermosFiles/Voenmech/ВКР/programming/dart_pkg_kuznechik/kuznechik';
+  const path = 'D:/Slermo/SlermosFiles/Voenmech/ВКР/programming/dart_pkg_kuznechik/kuznechik';
   Kuznechik(DynamicLibrary.open(path));
   var awesome = Awesome();
   print('awesome: ${awesome.isAwesome}');
